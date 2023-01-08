1adc59233a8f638e0ee26f83f5a9d56c097eeb03
Jasarat Zaheen <jasaratzaheen19@gmail.com>
2023-01-08 22:08:33 +0600

    my initials solution

---
+++ CHALLENGE 1/section/HelloJava.java
@@ -0,0 +1,17 @@
+public class HelloJava{
+     public static void main(String[] args) {
+        System.out.println("JJJJJ  ZZZZZ");
+        System.out.println("  J        Z");
+        System.out.println("  J       Z");       
+        System.out.println("  J      Z");
+        System.out.println("  J     Z");
+        System.out.println("J J    Z");
+        System.out.println(" JJ    ZZZZZ");
+         
+        
+
+        
+  
+        
+    }
+}
