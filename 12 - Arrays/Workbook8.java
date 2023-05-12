51068b4216e165193d45bfc79de1469f9da2b5b1
Jasarat Zaheen <Jasaratzaheen19@gmail.com>
2023-05-12 23:18:41 +0600

    array

---
+++ 12 - Arrays/Workbook8.java
@@ -0,0 +1,23 @@
+public class Workbook8 {
+    
+    public static void main(String[] args) {
+        String [] apple = {"Gala", "Granny Smith", "Macintosh"};
+        double  [] price = {1.99, 1.49, 1.29};
+
+
+        System.out.println("Here's your receipt:\n");
+        
+        // See instructions on Learn the Part (Workbook 6.8)
+        for(int i=0; i < apple.length; i++){
+            System.out.println(apple[i] + ": $" + price[i]); 
+        }
+        
+       
+    }
+
+    
+        
+       
+    }
+
+
