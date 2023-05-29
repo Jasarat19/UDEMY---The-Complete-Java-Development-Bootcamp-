0dfdfdcc7fc9538dc78e962b63a4909d3d93a036
Jasarat Zaheen <Jasaratzaheen19@gmail.com>
2023-05-29 21:34:40 +0600

    array

---
+++ 10 - Loops/Blackjack.java
@@ -0,0 +1,254 @@
+
+import java.security.Identity;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Blackjack {
+
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to Java Casino!");
+        System.out.println("Do you have a knack for Black Jack?");
+        System.out.println("We shall see..");
+        System.out.println("..Ready? Press anything to begin!");
+        //Task 3 – Wait for the user to press enter.
+        scan.nextLine();
+        //Task 4 – Get two random cards.
+        int cardNumber1 =drawRandomCard();
+        String randomCard1= card(cardNumber1);
+        int cardNumber2 =drawRandomCard();
+        String randomCard2= card(cardNumber2);
+        //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        
+        System.out.println("\n You get a \n "+ randomCard1 + "\n and a \n " + randomCard2);
+       
+
+        //Task 5 – Print the sum of your hand value.
+        //       – print: your total is: <hand value>
+        int handvalue =  cardNumber1 + cardNumber2;
+        System.out.println(" your total is:"+handvalue );
+
+        
+        //Task 6 – Get two random cards for the dealer.
+        //       – Print: The dealer shows \n" + <first card> + "\nand has a card facing down \n" + <facedown card>
+        //       – Print: \nThe dealer's total is hidden
+        int dealerCardNumber1 =drawRandomCard();
+        String dealerRandomCard1= card(dealerCardNumber1);
+        int dealerCardNumber2 =drawRandomCard();
+        String dealerRandomCard2= card( dealerCardNumber2);
+        String hiddenCard = faceDown();
+
+        System.out.println("\n The dealer shows \n "+ dealerRandomCard1 + "\n and has a card facing down \n " + hiddenCard);
+
+        System.out.println("\nThe dealer's total is hidden");
+
+       
+        //Task 8 – Keep asking the player to hit or stay (while loop).
+        //       1. Every time the player hits
+        //             – draw a new card.
+        //             – calculate their new total.
+        //             – print: (new line) You get a (new line) <show new card>.
+        //             - print: your new total is <total>
+
+        //       2. Once the player stays, break the loop. 
+
+        while(true){
+            String option = hitOrStay();
+       
+            if(option.equals("hit")){
+                int value = drawRandomCard();           
+                String card = card(value);
+                int total = handvalue + value; 
+                System.out.println("\n" + card);
+                System.out.println("your new total is" + total);
+
+
+            }
+            else if(option.equals("stay")) {
+                break;
+            }
+            
+        }
+        //For tasks 9 to 13, see the article: Blackjack Part II. 
+         scan.close();
+
+    }
+
+    /** Task 1 – make a function that returns a random number between 1 and 13
+     * Function name – drawRandomCard
+     * @return (int)
+     *
+     * Inside the function:
+     *   1. Gets a random number between 1 and 13.
+     *   2. Returns a card.
+     * 
+     */
+    public static int drawRandomCard() {
+        return (int) (Math.random() * 13 + 1);
+        
+    }
+
+    /** Task 2 – make a function that returns a String drawing of the card.
+     * Function name – cardString
+     * @param cardNumber (int)
+     * @return (String)
+     *
+     * Inside the function:
+     *   1. Returns a String drawing of the card.
+     */
+    public static String card(int cardNumber) {
+        if (cardNumber == 1) {
+            return "   _____\n" +
+                    "  |A _  |\n" +
+                    "  | ( ) |\n" +
+                    "  |(_'_)|\n" +
+                    "  |  |  |\n" +
+                    "  |____V|\n";
+
+        } else if (cardNumber == 2) {
+            return "   _____\n" +
+                    "  |A _  |\n" +
+                    "  | ( ) |\n" +
+                    "  |(_'_)|\n" +
+                    "  |  |  |\n" +
+                    "  |____V|\n";
+
+        } else if (cardNumber == 3) {
+            return "   _____\n" +
+                    "  |3    |\n" +
+                    "  | o o |\n" +
+                    "  |     |\n" +
+                    "  |  o  |\n" +
+                    "  |____E|\n";
+
+        } else if (cardNumber == 4) {
+            return "   _____\n" +
+                    "  |4    |\n" +
+                    "  | o o |\n" +
+                    "  |     |\n" +
+                    "  | o o |\n" +
+                    "  |____h|\n";
+
+        } else if (cardNumber == 5) {
+            return "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+
+        } else if (cardNumber == 6) {
+            return "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+
+        } else if (cardNumber == 7) {
+            return "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+
+        } else if (cardNumber == 8) {
+            return "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+
+        } else if (cardNumber == 9) {
+            return "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+
+        } else if (cardNumber == 10) {
+            return "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+
+        } else if (cardNumber == 11) {
+            return "   _____\n" +
+                    "  |J  ww|\n" +
+                    "  | o {)|\n" +
+                    "  |o o% |\n" +
+                    "  | | % |\n" +
+                    "  |__%%[|\n";
+
+        } else if (cardNumber == 12) {
+            return "   _____\n" +
+                    "  |Q  ww|\n" +
+                    "  | o {(|\n" +
+                    "  |o o%%|\n" +
+                    "  | |%%%|\n" +
+                    "  |_%%%O|\n";
+
+        } else if (cardNumber == 13) {
+            return "   _____\n" +
+                    "  |K  WW|\n" +
+                    "  | o {)|\n" +
+                    "  |o o%%|\n" +
+                    "  | |%%%|\n" +
+                    "  |_%%%>|\n";
+
+        }
+        return "";
+
+        
+
+    }
+
+
+
+
+
+    public static String faceDown() {
+        return
+        "   _____\n"+
+        "  |     |\n"+ 
+        "  |  J  |\n"+
+        "  | JJJ |\n"+
+        "  |  J  |\n"+
+        "  |_____|\n";
+    }
+    
+    /** Task 7 – make a function that asks the user to hit or stay.
+     * Function name – hitOrStay
+     * @return (String)
+     *
+     * Inside the function:
+     *   1. Asks the user to hit or stay.
+     *   2. If the user doesn't enter "hit" or "stay", keep asking them to try again by printing:
+     *      Please write 'hit' or 'stay'
+     *   3. Returns the user's option 
+     */
+
+     
+    public static String hitOrStay(){
+
+        System.out.println("Please, enter hit or stay");
+        
+        while(true){
+            String option = scan.nextLine();
+            if(!option.equals("hit") && !option.equals("stay")){
+                System.out.println("Please write 'hit' or 'stay'");
+                
+            }
+            else{
+            return option;
+            }
+    }
+
+    }
+}
--- 12 - Arrays/Javapedia.java
+++ 12 - Arrays/Javapedia.java
@@ -5,100 +5,92 @@ public class Javapedia {
         Scanner scan = new Scanner(System.in);
         System.out.println("\n**********Javapedia**********");
         System.out.println("How many historical figures will you register?");
-        //Task 1 – Ask the u(ser: how many historical figures will you register?
-        //       – Store the value.
+        // Task 1 – Ask the u(ser: how many historical figures will you register?
+        // – Store the value.
         int figures = scan.nextInt();
-        
-        //Task 2 – Create a 2D array with a variable number of rows, and 3 values per row.         
-        String [][] arrays = new String [figures][3] ;
-        //Watch out for the nextLine() pitfall.
-        scan.nextLine(); 
-        // Task 3 
-        for (int i =0; i<arrays.length; i++) {
 
-            System.out.println("\n\tFigure " + (i+1)); 
-            for(int j =0; j<arrays[i].length;j++){
-                if(j==0) {
+        // Task 2 – Create a 2D array with a variable number of rows, and 3 values per
+        // row.
+        String[][] arrays = new String[figures][3];
+        // Watch out for the nextLine() pitfall.
+        scan.nextLine();
+        // Task 3
+        for (int i = 0; i < arrays.length; i++) {
+
+            System.out.println("\n\tFigure " + (i + 1));
+            for (int j = 0; j < arrays[i].length; j++) {
+                if (j == 0) {
                     System.out.print("\t - Name: ");
                     String name = scan.nextLine();
-                    arrays[i][j]= name; 
-                }
-                else if(j==1){
+                    arrays[i][j] = name;
+                } else if (j == 1) {
                     System.out.print("\t - Date of birth: ");
                     String date = scan.nextLine();
-                    arrays[i][j]= date;
+                    arrays[i][j] = date;
 
-                }
-                else if(j==2){
+                } else if (j == 2) {
                     System.out.print("\t - Occupation: ");
-                    String occu = scan.nextLine(); 
-                     arrays[i][j]= occu;
+                    String occu = scan.nextLine();
+                    arrays[i][j] = occu;
 
                 }
-                   
-                    
-                }
+
+            }
 
             System.out.print("\n");
         }
 
-        
-        
-      
-        System.out.println("These are the values you stored:"); 
+        System.out.println("These are the values you stored:");
         print2DArray(arrays);
 
-        System.out.print("\nWho do you want information on? ");  
-        
-        /*Task 5: Let the user search the database by name. 
-            If there's a match:
-              print(    tab of space    Name: <name>)
-              print(    tab of space    Date of birth: <date of birth>)
-              print(    tab of space    Occupation: <occupation>)
+        System.out.print("\nWho do you want information on? ");
 
-        */   
-        
-        String search =scan.nextLine(); 
-        for(int i =0; i<arrays.length;i++ ){
-            System.out.print("\t");
-            for (int j = 0;j<arrays[i].length;j++){
-                System.out.println(arrays[i][0]);
-                if(search.equals(arrays[i][0])){
-                    System.out.print( "\tName:" + arrays[i][j]) ;
-                    System.out.print("\tDate of birth:" + arrays[i][j+1]); 
-                    System.out.print("\tOccu:" + arrays[i][j+2]); 
+        /*
+         * Task 5: Let the user search the database by name.
+         * If there's a match:
+         * print( tab of space Name: <name>)
+         * print( tab of space Date of birth: <date of birth>)
+         * print( tab of space Occupation: <occupation>)
+         * 
+         */
 
-                    
-                }
-                else{
-                    continue;
-                }
+        String search = scan.nextLine();
+        for (int i = 0; i < arrays.length; i++) {
+            
+            if (search.equals(arrays[i][0])) {
+                System.out.println("\tName:" + arrays[i][0]);
+                System.out.println("\tDate of birth:" + arrays[i][1]);
+                System.out.println("\tOccu:" + arrays[i][2]);
+
+            } else {
+                continue;
             }
+
         }
-        
 
         scan.close();
     }
 
-   /*print2DArray
+    /*
+     * print2DArray
+     * 
      * @param array (String[][])
      * 
      * Inside the function
-     *  1. print the database
-     *     • a tab of space precedes each row.
-     *     • each value in database has one space from the other value. 
-     *     • print a new line.
+     * 1. print the database
+     * • a tab of space precedes each row.
+     * • each value in database has one space from the other value.
+     * • print a new line.
      */
 
-     public static void print2DArray(String[][] arrays) {
-        for(int i =0; i<arrays.length;i++ ){
+    public static void print2DArray(String[][] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
             System.out.println("\t");
-            for (int j = 0;j<arrays[i].length;j++){
-                System.out.print(arrays[i][j]+" ");
+            for (int j = 0; j < arrays[i].length; j++) {
+                System.out.print(arrays[i][j] + " ");
             }
         }
-        
-     }
+
+    }
 
 }
-    
